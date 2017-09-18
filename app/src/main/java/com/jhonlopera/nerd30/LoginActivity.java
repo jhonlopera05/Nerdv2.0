7237132f7d9b66026ac08ae1e94b8cabe987c8ef
jhonlopera05 <jhon.lopera05@gmail.com>
2017-09-18 16:58:46 -0500

    Version 2.0

--- app/src/main/java/com/jhonlopera/nerd30/LoginActivity.java
+++ app/src/main/java/com/jhonlopera/nerd30/LoginActivity.java
@@ -2,25 +2,86 @@ package com.jhonlopera.nerd30;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class LoginActivity extends AppCompatActivity {
     private String correoR,contraseñaR, correo, contraseña;
     private EditText ecorreo, econtraseña;
     int duration = Toast.LENGTH_SHORT;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.jhonlopera.nerd30",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
         ecorreo = (EditText) findViewById(R.id.eCorreo);
         econtraseña = (EditText) findViewById(R.id.eContraseña);
+
+        loginButton=(LoginButton) findViewById(R.id.login_button);
+
+        callbackManager= CallbackManager.Factory.create();
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(),"Login exitosoo",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                //intent.putExtra("correo",correoR);
+                //intent.putExtra("contraseña",contraseñaR);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),"Login cancelado",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"Error en el login",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         Bundle extras= getIntent().getExtras();
 
@@ -46,6 +107,7 @@ public class LoginActivity extends AppCompatActivity {
             intent.putExtra("correo",correoR);
             intent.putExtra("contraseña",contraseñaR);
             startActivity(intent);
+            finish();
         }
         else{
             Context context = getApplicationContext();
@@ -58,11 +120,13 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Toast.makeText(this,"hola",Toast.LENGTH_SHORT);
         if (requestCode==1234 && resultCode==RESULT_OK){
             correoR=data.getExtras().getString("correo");
             contraseñaR=data.getExtras().getString("contraseña");
             Toast.makeText(this,correoR,Toast.LENGTH_SHORT);
+        }
+        else {
+            callbackManager.onActivityResult(requestCode,resultCode,data);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
